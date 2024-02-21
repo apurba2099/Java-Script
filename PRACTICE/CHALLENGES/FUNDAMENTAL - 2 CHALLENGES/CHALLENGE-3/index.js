d31cdfae2812f3e319ad8d0cf8da507ac38b2176
Apurba Dutta <apurbdutta2099@gmail.com>
2024-02-21 12:56:56 +0530

    practice

--- PRACTICE/CHALLENGES/FUNDAMENTAL - 2 CHALLENGES/CHALLENGE-3/index.js
+++ PRACTICE/CHALLENGES/FUNDAMENTAL - 2 CHALLENGES/CHALLENGE-3/index.js
@@ -1,28 +1,28 @@
-//test data
-
-const apurba = {
-    MyName:"Apurba Dutta",
-    roll:65,
-    class:"cst",
-    college: "jis"
+"use strict;"
+//BMI function
+calcBMI = function (mass, height){
+  return mass / height**2;
 }
-console.log(apurba);
-console.log(Object.values(apurba))
-console.log(Object.keys(apurba))
-const newStart = "My"
-const add = "Name"
-console.log(["My"+ add])
-// console.log(apurba["My"+add])
 
-
-const biku = {
-  firstName: "Apurba",
-  lastName: "Dutta",
-  age: 2024 - 2002,
-  work: "Coder",
-  friends: ["Michael", "Peter", "Steven"],
+//Mark Miller details
+const mark = {
+  weight:92,
+  tall:1.95
 };
+const markBMI = calcBMI(mark.weight,mark.tall);
+console.log(`Mark Miller BMI is: ${markBMI.toFixed(2)}`);
 
-const nameKey = "Name";
-console.log(biku["first" + nameKey]);
-console.log(biku["last" + nameKey]);
+//John Smith details
+const john = {
+  weight:78,
+  tall:1.69
+};
+const johnBMI = calcBMI(john.weight,john.tall);
+console.log(`John Smith BMI is: ${johnBMI.toFixed(2)}`);
+
+if(markBMI > johnBMI){
+  console.log(`"Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!"`)
+}
+else{
+  console.log(`"John's BMI (${johnBMI.toFixed(2)}) is higher than Marks's (${markBMI.toFixed(2)})!"`)
+}
