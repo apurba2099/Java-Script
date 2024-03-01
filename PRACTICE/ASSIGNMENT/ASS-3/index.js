67ed22da2e8decb72199dab725a52f35e0f499d9
Apurba Dutta <apurbadutta2099@gmail.com>
2024-03-01 21:53:07 +0530

    practice

--- PRACTICE/ASSIGNMENT/ASS-3/index.js
+++ PRACTICE/ASSIGNMENT/ASS-3/index.js
@@ -1,4 +1,5 @@
 //L. functions
+console.log('//L. functions//')
 const describeCountry = function (country, population, capitalCtiy){
    return `'${country} has ${population} million people and its capital city is ${capitalCtiy}'.`
 };
@@ -11,9 +12,9 @@ console.log(description2);
 //different country
 const description3 = describeCountry('United state America', 30, 'Washinton DC');
 console.log(description3);
-console.log('/////////////////////////');
 /////////////////////////////////////
 //M. General Functions vs. Expressions
+console.log('//M. General Functions vs. Expressions//')
 const percentageOfWorld1 = function (population){
     const data = (population/7900)*100;
     return data;
@@ -27,9 +28,9 @@ console.log(`India has 1437 million people, so it's about ${secondCountry.toFixe
 //3rd Country Data
 let thirdCountry = percentageOfWorld1(336);
 console.log(`Unite State has 336 million people, so it's about ${thirdCountry.toFixed(2)}% of the world's population.`)
-console.log('/////////////////////////');
 ////////////////////////////////////
 //N. Arrow Functions
+console.log('//N. Arrow Functions//')
 percentageOfWorld3 = (population) =>{
     return (population/7900)*100;
 };
@@ -42,9 +43,9 @@ console.log(`India has 1437 million people, so it's about ${secondCountry.toFixe
 //3rd Country Data
 thirdCountry = percentageOfWorld3(336);
 console.log(`Unite State has 336 million people, so it's about ${thirdCountry.toFixed(2)}% of the world's population.`);
-console.log('/////////////////////////');
 ////////////////////////////////////
 //O. Functions Calling Other Functions
+console.log('//O. Functions Calling Other Functions//')
 const describePopulation = function (country, population){
     const percentage= (population / 7900) * 100;
     return `${country} has ${population} million people, which is about ${percentage.toFixed(2)}% of the world.`;
@@ -58,9 +59,9 @@ console.log(secondCountry);
 //3rd Country
 thirdCountry = describePopulation("America", 336)
 console.log(thirdCountry);
-console.log('/////////////////////////');
 ////////////////////////////////////
 //P. Introduction to Arrays
+console.log('//P. Introduction to Arrays//')
 const population = [1441, 1437, 336, 172];
 const lengthpopulation = population.length === 4;
 
@@ -72,6 +73,7 @@ const joinPercentage = percentage.join("% ")
 console.log(`Here the all percentage the given array: ${joinPercentage}%`);
 ////////////////////////////////////
 //Q. Basic Array Operations (Methods)
+console.log('//Q. Basic Array Operations (Methods)//')
 const neighbours = ['China', 'Srilanka', 'Nepal', 'Bhutan'];
 console.log(`Before adding new country: ${neighbours}`)
 neighbours.push('Utopia');
@@ -88,22 +90,74 @@ console.log(`After Dissolved new country: ${neighbours}`);
 if (!neighbours.includes("Germany")) {
     console.log("Probably not a central European country.");
 }
-// Step 5: Change the name of one neighbouring country
 const indexToReplace = neighbours.indexOf("Netherlands");
 if (indexToReplace !== -1) {
     neighbours[indexToReplace] = "Republic of Sweden";
 }
 console.log(neighbours);
-
+////////////////////////
 // R. Introduction to Objects
- const myCountry = {
-     'Country':'India',
-     'Captial':'New Delhi',
-     'language':'English',
-     'Population':'150',
-     'Neighbours':['China','Bangladesh','Pakistan']
+console.log('// R. Introduction to Objects//')
+ let myCountry = {
+     Country:'India',
+     Captial:'New Delhi',
+     language:'English',
+     Population:'150',
+     Neighbours:['China','Bangladesh','Pakistan']
  };
  console.log(myCountry) ;
+/////////////////
+//S. Dot vs. Bracket Notation
+console.log('////S. Dot vs. Bracket Notation////')
+ let finland = {
+    name: "Finland",
+    population: 6,//million
+    language: "Finnish",
+    neighbouringCountries: 3,
+    capital: "Helsinki"
+};
+console.log(`${finland.name} has ${finland.population} million ${finland.language} speaking people, ${finland.neighbouringCountries} neighbouring countries and a capital called ${finland.capital}.`);
+
+const increaseCountryPopulation = finland.population + 2;
+const decreaseCountryPopulation = finland['population'] - 2;
+
+console.log(`The increase country population by two million is: ${increaseCountryPopulation} millions`);
+console.log(`The decrease country population by two million is: ${decreaseCountryPopulation} millions`);
+console.log('//////////////////////')
+/////////////////////////
+//T. Object Methods
+console.log('////T. Object Methods////')
+   myCountry = {
+    Country: 'India',
+    Capital: 'New Delhi',
+    Language: 'English',
+    Population: 150,
+    Neighbours: ['China', 'Bangladesh', 'Pakistan'],
+    describe: function() {
+        console.log(`${this.Country} has ${this.Population} million ${this.Language} speaking people, ${this.Neighbours.length} neighbouring countries and a capital called ${this.Capital}.`);
+    },
+    checkIsland: function() {
+        this.isIsland = this.Neighbours.length === 0 ? true : false;
+    }
+};
+myCountry.describe();
+myCountry.checkIsland();
+console.log(myCountry.isIsland);
+
+/////////////////////////
+//U. Iteration: The for Loop
+console.log('////U. Iteration: The for Loop////')
+for(let i=1; i<=50; i++){
+    console.log(`Voter number ${i} is currently voting`);
+}
+/////////////////////////
+//V. Looping Arrays, Breaking and Continuing
+console.log('////V. Looping Arrays, Breaking and Continuing ////')
+
+
+
+
+
  
 
 
