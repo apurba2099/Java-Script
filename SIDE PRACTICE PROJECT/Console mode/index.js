1786741e99d08e4ec239abaa736b432211b21d01
Apurba Dutta <apurbadutta2099@gmail.com>
2024-03-10 20:06:56 +0530

    some console method

---
+++ SIDE PRACTICE PROJECT/Console mode/index.js
@@ -0,0 +1,15 @@
+const a = ['apurba','dutta','roll','class']
+console.table(a);
+console.log(a);
+
+let obj = {
+    fname:'apurba',
+    lname:'dutta',
+    class:'cst',
+    sem:'sem4'
+};
+console.log(obj);
+console.table(obj);
+
+console.warn("hello world");
+console.error('error');
