e75b5a87ffd02535095c9e43f3efd8ba3a3061da
Apurba Dutta <apurbadutta2099@gmail.com>
2024-03-17 19:35:33 +0530

    update

--- CLASS/3. DOM MANUPULATION/Project-1/Guess  The Number/script.js
+++ CLASS/3. DOM MANUPULATION/Project-1/Guess  The Number/script.js
@@ -10,5 +10,8 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   document.querySelector('.number').textContent = guess;
-   
+   console.log(guess);
+   if(!guess){
+    document.querySelector(".message").textContent="No number!"
+   }
 });
