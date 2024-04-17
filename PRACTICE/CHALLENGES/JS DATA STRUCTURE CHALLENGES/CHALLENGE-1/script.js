6d9a421179667453899e243397e17fca07ac44ff
Apurba Dutta <apurbadutta2099@gmail.com>
2024-04-17 23:04:05 +0530

    add

--- PRACTICE/CHALLENGES/JS DATA STRUCTURE CHALLENGES/CHALLENGE-1/script.js
+++ PRACTICE/CHALLENGES/JS DATA STRUCTURE CHALLENGES/CHALLENGE-1/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//question destructuring
+//Given API DATA
 const books = [
   {
     title: "Algorithms",
@@ -226,5 +226,35 @@ const books = [
   },
 ];
 
+//START
 
-//START 
+// 1.1
+// Destructure the books array into two variables called firstBook and secondBook
+
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+
+// 1.2
+// Destructure the books array into a variable called thirdBook. You must skip the first two books.
+
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+// 1.3
+// Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the rating variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+
+const ratings = [
+  ["rating", 4.19],
+  ["ratingsCount", 144584],
+];
+
+const [[i, j], [a, b]] = ratings;
+console.log(i, j, a, b);
+
+// 1.4
+// Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+
+const ratingStars = [63405, 1808];
+const [fiveStarRatings, oneStarRatings, threeStarratings = 0] = ratingStars;
+
+console.log(fiveStarRatings, oneStarRatings, threeStarratings);
