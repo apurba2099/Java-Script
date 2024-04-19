acb70edc1d735e9b9f0b8c5e9009c90fa81d21e3
Apurba Dutta <apurbadutta2099@gmail.com>
2024-04-19 23:53:52 +0530

    update

--- PRACTICE/ASSIGNMENT/2. JS DATA STRUCTURE ASSIGNMENT/ASS-2/script.js
+++ PRACTICE/ASSIGNMENT/2. JS DATA STRUCTURE ASSIGNMENT/ASS-2/script.js
@@ -256,14 +256,24 @@ const [
     keywords: [mainKeyword, ...reset],
   },
 ] = books;
-console.log(mainKeyword,reset);
+console.log(mainKeyword, reset);
 
 // 4.2
 // Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
 
-const [
-  {
-    keywords: [mainKeywor, ...reset],
-  },
-] = books;
-console.log(mainKeyword, reset);
+const [, { publisher, ...resetOfTheBook }] = books;
+const bookPublisher = publisher;
+console.log(bookPublisher);
+// console.log(resetOfTheBook); //just for check
+
+// 4.3
+// Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+// Example:
+// printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+// Expected Output:
+// "The book "Algorithms" has 2 authors"
+
+const printBookAuthorsCount = function (title, ...author) {
+  console.log(`"The book ${title} has ${author.length} authors"`);
+};
+printBookAuthorsCount("Algorithms", "Robert", "sedgewick");
