1d7fc9ad46672cf181cb15f7db78496eeef8bbe4
Apurba Dutta <apurbadutta2099@gmail.com>
2024-04-22 23:13:08 +0530

    add classes

---
+++ CLASS/5. JS DATA STRUCTURE/3. Looping Array For Of loop/script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const resturant = {
+  rname: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "15:00",
+    address,
+  }) {
+    console.log(
+      `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+// Looping Arrays: The for-of loop
+const menu = [...resturant.starterMenu, ...resturant.mainMenu];
+
+// for (let i = 0; i < menu.length; i++) {
+//   console.log(menu[i]);
+// }
+
+for (const item of menu) console.log(item);
+
+// for (const item of menu) console.log(menu.indexOf(item));
+
+for (const item of menu.entries()) console.log(item);
+
+// console.log([...menu.entries()]);
+
+// Example
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
