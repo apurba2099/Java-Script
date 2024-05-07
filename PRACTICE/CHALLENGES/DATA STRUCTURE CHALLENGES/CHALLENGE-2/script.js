17e73a93ce7e99be3e17f072fea77eec8b3aaeee
Apurba Dutta <apurbadutta2099@gmail.com>
2024-05-07 20:26:46 +0530

    practice challenge-2

--- PRACTICE/CHALLENGES/DATA STRUCTURE CHALLENGES/CHALLENGE-2/script.js
+++ PRACTICE/CHALLENGES/DATA STRUCTURE CHALLENGES/CHALLENGE-2/script.js
@@ -40,3 +40,26 @@ const game = {
     team2: 6.5,
   },
 };
+// task: 1
+let goal = 0;
+for (const score of game.scored) {
+  console.log(`Goal ${++goal}: ${score}`);
+}
+// task: 2
+const {
+  odds: { team1: first, x: draw, team2: second },
+} = game;
+console.log(`${(first + draw + second / 3).toFixed(2)}% Average`);
+
+// task: 3
+const { team1, team2 } = game;
+console.log(
+  `Odd of victory ${team1}: ${first}\n Odd of draw: ${draw}\n  Odd of victory ${team2}: ${second}`
+);
+
+// task: 4
+const scorers = {
+  Gnarby: 1,
+  Hummels: 1,
+  Lewandowski: 2,
+};
