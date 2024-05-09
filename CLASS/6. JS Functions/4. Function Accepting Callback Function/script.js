5c6527508eb4b75116fa1a90ba8a78bf404edef3
Apurba Dutta <apurbadutta2099@gmail.com>
2024-05-09 21:20:31 +0530

    add class: function accepting call back function

---
+++ CLASS/6. JS Functions/4. Function Accepting Callback Function/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Function Accepting Callback Function
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+const high5 = function () {
+  console.log("👋");
+};
+
+document.querySelector(".buy").addEventListener("click", high5);
+
+["Biku", "Ram", "Shayam"].forEach(high5);
