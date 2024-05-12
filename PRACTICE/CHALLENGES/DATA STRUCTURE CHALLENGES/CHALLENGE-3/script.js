da4f58af8712c9abfe01cf0a6ca92f98d125fd0b
Apurba Dutta <apurbadutta2099@gmail.com>
2024-05-12 11:30:19 +0530

    daily practice

---
+++ PRACTICE/CHALLENGES/DATA STRUCTURE CHALLENGES/CHALLENGE-3/script.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// task: 1
+const events = new Set(gameEvents.values());
+console.log("Unique Events", events);
+
+// task: 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// task: 3
+const numberOfEvents = gameEvents.size;
+const averageTime = 90 / numberOfEvents;
+console.log(`An event happened, on average, every ${averageTime} minutes`);
+
+// task: 4
+for (const [minute, event] of gameEvents.entries()) {
+  const time = minute <= 45 ? "FIRST HALF" : "SECOND HALF";
+  console.log(`[${time}] ${minute}: ${event}`);
+}
