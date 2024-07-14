45789609bf3c4f451c6f0a388d8733df95dcc651
Apurba Dutta <apurbadutta2099@gmail.com>
2024-07-14 21:56:02 +0530

    update

--- PRACTICE/CHALLENGES/DATA STRUCTURE CHALLENGES/CHALLENGE-4/script.js
+++ PRACTICE/CHALLENGES/DATA STRUCTURE CHALLENGES/CHALLENGE-4/script.js
@@ -1,56 +1,22 @@
 "use strict";
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-const gameEvents = new Map([
-  [17, "⚽ GOAL"],
-  [36, "🔁 Substitution"],
-  [47, "⚽ GOAL"],
-  [61, "🔁 Substitution"],
-  [64, "🔶 Yellow card"],
-  [69, "🔴 Red card"],
-  [70, "🔁 Substitution"],
-  [72, "🔁 Substitution"],
-  [76, "⚽ GOAL"],
-  [80, "⚽ GOAL"],
-  [92, "🔶 Yellow card"],
-]);
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("button"));
+// document.body.append(document.createElement("textarea"));
+// document.body.append(document.createElement("button"));
+
+const submitBtn = document.querySelector(".btn");
+const textarea = document.querySelector(".areatext").value;
+
+submitBtn.addEventListener("click", function () {
+  const text = document.getElementById("textarea").value;
+  const lines = text.split("\n");
+
+  for (let line of lines) {
+    if (line.trim()) {
+      const words = line.trim().toLowerCase().split("_");
+      let camelCase = words[0];
+      for (let i = 1; i < words.length; i++) {
+        camelCase += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+      console.log(camelCase);
+    }
+  }
+});
