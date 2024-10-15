7843931e263d652e215eb41130994728f93d7859
Apurba Dutta <apurbadutta2099@gmail.com>
2024-10-15 20:35:59 +0530

    update

--- CLASS/9. OOP with Javascript/All OOP Code/index.js
+++ CLASS/9. OOP with Javascript/All OOP Code/index.js
@@ -8,6 +8,8 @@
 // 4. function automatically return {}.
 
 // THIS IS THE BLUE PRINT OF THESE OBJECTS
+
+/*
 const Person = function (firstName, lastname, birthYear) {
   //   console.log(this);
   this.firstName = firstName;
@@ -60,7 +62,9 @@ Person.prototype.species = "Homo Sapiens";
 console.log(apurba.species, avishek.species, peter.species);
 console.log(apurba.hasOwnProperty("firstName"));
 console.log(apurba.hasOwnProperty("species"));
+*/
 
+/*
 //212. no video
 console.log();
 console.log(apurba.__proto__.__proto__);
@@ -79,8 +83,87 @@ console.log(arr.unique());
 
 const h1 = document.querySelector("h1");
 console.dir(h1);
+*/
 
+/*
 // 214. no video
 //ES6 class
+// Let class declaration
+class PersonClass {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to prototype property
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+  greet() {
+    console.log(`hello! ${this.firstName}`);
+  }
+}
+const apurba = new PersonClass("Apurba Dutta", 2003);
+const avishek = new PersonClass("Avishek Dutta", 1996);
+// console.log(apurba)
+// add method
+apurba.calcAge();
+console.log(PersonClass.prototype === apurba.__proto__);
+apurba.greet();
+avishek.greet();
 
+// 1. classes are NOT hoisted 
+// 2. Class are first-class citizens (class are special kind of functions)
+// 3. classes are executed in strict mode
+*/
 
+// 215 no video
+// Setters and Getters
+
+const account = {
+  owner: "apurba",
+  movements: [10, 20, 300, 4300, 20],
+  birthYear: 2003,
+  // get
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  // Set
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+
+  // get using age method
+  get age() {
+    return 2024 - this.birthYear;
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+console.log(account.latest);
+console.log(account.age);
+
+// for better understanding here the example
+
+class details {
+  constractor(fullName, birthyear) {
+    this.fullName = fullName;
+    this.birthyear = birthyear;
+  }
+
+  set fullName(name) {
+    // console.log(name);
+    if (name.includes("")) {
+      this._fullName = name;
+    } else {
+      alert(`${name} not a full name`);
+    }
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+//correction needed
+const apurbaD = new details("Apurba Dutta", 2003);
+const avishekD = new details("Avishek Dutta", 1996);
+console.log(apurbaD.fullName);
