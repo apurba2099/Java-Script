a93f2c91c1b121068edcc26e5f92d3758241adf0
Apurba Dutta <apurbadutta2099@gmail.com>
2024-10-20 19:48:31 +0530

    challenege 2 ES6 and getter and setter solve to undarstand

--- PRACTICE/CHALLENGES/OOP/OOP - 2 CHALLENGE/index.js
+++ PRACTICE/CHALLENGES/OOP/OOP - 2 CHALLENGE/index.js
@@ -1,35 +1,60 @@
 "use strict";
-// challeneg 1 oops
-// const Car = function (brand, speed) {
-//   this.brand = brand;
-//   this.speed = speed;
-// };
-// const bmw = new Car("BMW", 150);
-// const marcedes = new Car("Marcedes", 100);
-// const pulsar = new Car("Pulsar", 180);
+// challeneg 2 oops
+class Carlc {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
 
-// //create a speed increase method
-// Car.prototype.increase = function () {
-//   this.speed += 10;
-//   console.log(`${this.brand} is speed ${this.speed} k/h`);
-// };
-// //create a speed decrease method
-// Car.prototype.decrease = function () {
-//   this.speed -= 5;
-//   console.log(`${this.brand} is speed ${this.speed} k/h`);
-// };
-// console.log(bmw);
-// console.log(marcedes);
-// console.log(pulsar);
-// bmw.increase();
-// bmw.decrease();
+  //create a speed increase method
+  accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is speed ${this.speed} k/h`);
+  };
+  //create a speed decrease method
+  brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is speed ${this.speed} k/h`);
+  };
 
-// marcedes.increase();
-// marcedes.decrease();
+  get speedUS() {
+    return this.speed / 1.6;
+  }
 
-// pulsar.increase();
-// pulsar.decrease();
-
-class CarCl {
-  constructor() {}
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
 }
+
+const ford = new Carlc("Ford", 120);
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
+
+///This is more specified to understanding the getter and setter
+class Details {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  //  fullName(name) { this is a function
+  set fullName(name) {
+    if (name.includes(" ")) {
+      [this.firstName, this.lastName] = name.split(" ");
+    } else {
+      console.log(`This ${name} is not a valid name`);
+    }
+  }
+  // fullName() { this is function
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const apurbaD = new Details("Apurba", "Dutta", 2003);
+console.log(apurbaD.fullName);
+apurbaD.fullName = 'Apurba Roy'
+console.log(apurbaD.fullName);
+console.log(typeof apurbaD.fullName);
