e87dd432dd16f5741cd118fd88fa947bcf879072
Apurba Dutta <apurbadutta2099@gmail.com>
2024-10-29 11:03:32 +0530

    Encapsulation private and public method and fields

--- CLASS/9. OOP with Javascript/All OOP Code/index.js
+++ CLASS/9. OOP with Javascript/All OOP Code/index.js
@@ -503,3 +503,89 @@ acc1.approveLoan(100);
 console.log(acc1.getMovements());
 */
 
+/*
+//225. no video
+// Encapsulation: private class Fields and methods
+// // console.log(acc1.#pin) Private field '#movements' must be declared in an enclosing class
+// // 1) Public fields
+// // 2) Private fields
+// // 3) Public methods
+// // 4) Private methods
+// // (there is also the static version)
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    // Protected property
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+
+// Note: //error message which is : Private field '#movements' must be declared in an enclosing class (we can not access to out side)
+
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+*/
