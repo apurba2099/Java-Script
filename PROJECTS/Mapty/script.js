bbbba00b989a2f056b7f092b9197b5e24c587200
Apurba Dutta <apurbadutta2099@gmail.com>
2024-10-29 21:59:10 +0530

    navigator and geolocation.getCurrentPosition

--- PROJECTS/Mapty/script.js
+++ PROJECTS/Mapty/script.js
@@ -10,3 +10,24 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//geolocation use
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      //   console.log(position);
+      console.log(latitude, longitude);
+
+      //   use google maps links
+      console.table(
+        `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI0MTAyMy4wIKXMDSoASAFQAw%3D%3D}`
+      );
+    },
+    function () {
+      alert('Could not get your position!');
+    }
+  );
