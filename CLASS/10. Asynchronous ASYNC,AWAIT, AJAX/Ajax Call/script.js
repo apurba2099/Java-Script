5a9ebe57ea0ced73fd24dbd3f1302cac3d660f3f
Apurba Dutta <apurbadutta2099@gmail.com>
2024-11-28 16:20:46 +0530

    AJAX neighbour country

--- CLASS/10. Asynchronous ASYNC,AWAIT, AJAX/Ajax Call/script.js
+++ CLASS/10. Asynchronous ASYNC,AWAIT, AJAX/Ajax Call/script.js
@@ -1,33 +1,18 @@
 "use strict";
 
+//249. our first AJAX call : XMLHttpRequest
+
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 //////////////////////
 
-//makes all of the function on it!! 😃😃
-const getCountryData = function (country) {
-  // old skool way to call AJAX
-  const request = new XMLHttpRequest();
-
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-
-  request.send();
-
-  // write to print the JSON file see
-  // console.log(request.responseText);
-
-  request.addEventListener("load", function () {
-    //destructering
-    const [data] = JSON.parse(this.responseText);
-
-    console.log(data);
-
-    const html = `<article class="country">
+const renderCountry = function (data, className = " ") {
+  const html = `<article class="country ${className}">
   
           <img class="country__img" src="${data.flags.png}" alt="Flag of ${
-      data.name.common
-    }" />
+    data.name.common
+  }" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
             <h4 class="country__region">${data.region}</h4>
@@ -45,11 +30,67 @@ const getCountryData = function (country) {
           </div>
         </article>`;
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+//Makes all of the function on it.
+const getCountryAndNeighbour = function (country) {
+  //Old skool way to call AJAX
+
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  // print in terminal the JSON file came through API
+  // console.log(request.responseText);
+
+  request.addEventListener("load", function () {
+    //destructering
+    const [data] = JSON.parse(this.responseText);
+    //to see the data in object format
+    console.log(data);
+
+    // calling the country data Render country 1
+    renderCountry(data);
+
+    ////Get neighbour country (2)///
+
+    // const [neighbour] = data.borders;
+    // if (!neighbour) return;
+    // instead of
+    // use optional chaining for better managable error code:
+
+    // 1st using nulls coalsing
+    // const borders = data.borders ?? [];
+    // const [neighbour] = borders;
+
+    // 2nd optional chainging
+    const neighbour = data.borders?.[0];
+    console.log(neighbour);
+
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      // console.log(this.responseText);
+
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // calling the neighbour country
+      renderCountry(data2, "neighbour");
+    });
   });
 };
 
-getCountryData("india");
-getCountryData("bangladesh");
-getCountryData("sri");
+// getCountryData("india");
+// getCountryData("bangladesh");
+// getCountryData("sri");
+
+///251 Callback Hell
+getCountryAndNeighbour("india");
+// getCountryAndNeighbour("Iceland");
