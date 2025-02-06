df61b082d2154bb95994cf627c97adabe06f95fa
Apurba Dutta <apurbadutta2099@gmail.com>
2025-02-06 17:37:30 +0530

    some command line

---
+++ CLASS/11. Mordern Js Development/5. Concept of Command line (NPM)/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//The Foundation of Command Line
+// Command:-
+// >> dir - directory
+// >> cd - change in-direction 
+// >> cd.. - backyard-direction
+// >> mkdir - make a directorie
+// >> clear - clear the terminal
+// >> touch - make a file
+// >> mv - move
+// >> rmdir - remove a directorie(its only use empty directory)
+// >> del - delete
