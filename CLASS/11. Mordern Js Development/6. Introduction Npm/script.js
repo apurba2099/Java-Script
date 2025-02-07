22481cedaad573ef438d7da79d53561c9fa23084
Apurba Dutta <apurbadutta2099@gmail.com>
2025-02-07 20:22:48 +0530

    add some npm concepts

--- CLASS/11. Mordern Js Development/6. Introduction Npm/script.js
+++ CLASS/11. Mordern Js Development/6. Introduction Npm/script.js
@@ -1,2 +1,30 @@
 "use strict";
 
+// Note: This section basically we learn whta is NPM, So NPM is stands for (NODE PACKAGE MANAGER)
+
+// SO all the terminal code written here -
+
+// > npm -v (-v version check).
+// > npm init (init install the npm)
+// use leaflet library by useing npm.
+
+// Lodash.com
+// npm i lodash-es
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+  cart: [
+    { product: "bread", quanity: 12 },
+    { product: "pizza", quanity: 12 },
+  ],
+  user: { loggedIn: true },
+};
+// Object.assign() use to clone, merge
+//No, Object.assign() does not perform a deep clone. It only performs a shallow copy,
+const stateClone = Object.assign({}, state);
+console.log(state); //also flase
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone); //deep clone
+state.user.loggedIn = false;
+console.log(stateClone);//shallow copy
